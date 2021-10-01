dfa6114258d63e245272f2fe45288d4eb72c8959
Luka Leer <mahtaran@amuzil.com>
2021-10-01 12:47:19 +0200

    Add Freemarker template engine
    Signed-off-by: Mahtaran <mahtaran@amuzil.com>

--- app/src/main/java/com/amuzil/mahtaran/nattynal/NattyNal.java
+++ app/src/main/java/com/amuzil/mahtaran/nattynal/NattyNal.java
@@ -1,5 +1,12 @@
 package com.amuzil.mahtaran.nattynal;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
 import static spark.Spark.*;
 
 public class NattyNal {
@@ -7,5 +14,13 @@ public class NattyNal {
 	public static void main(String[] args) {
 		port(12345);
 		get("/hello", (request, response) -> "Hello World!");
+		get("/", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			model.put("message", "Hello World!");
+			model.put("greeting", "General Kenobi");
+			Configuration config = new Configuration(Configuration.VERSION_2_3_26);
+			config.setClassForTemplateLoading(NattyNal.class, "/");
+			return new FreeMarkerEngine(config).render(new ModelAndView(model, "index.ftl"));
+		});
 	}
 }
---
+++ app/src/main/resources/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>${message}</title>
+	</head>
+	<body>
+		<h1>Hello there</h1>
+		<p>${greeting}</p>
+	</body>
+</html>
